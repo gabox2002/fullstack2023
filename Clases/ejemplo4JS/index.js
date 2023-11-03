e310b6ae8c4abd43aa0d73d41cf61e2e1ec2fa7c
gabox2002 <gabox2002@gmail.com>
2023-11-02 22:10:16 -0300

    clase principios js

---
+++ Clases/ejemplo4JS/index.js
@@ -0,0 +1,23 @@
+// Las variables declaradas como "var"van a poder accederse desde cualquier lugar del archivo js y podrán cambiar su valor y redefinirse
+var cadena = "Esta es una cadena de caracteres y la vamos a llamar STRING";
+// cadena = 5;
+
+// Las variables declaradas como "let" solo se va a poder acceder dento de un bloque de codigo se van a poder redeclarar y cambiar su valor
+let numero = 4;
+//numero = false;
+
+// la variables declaradas como "const" solo se va a poder redefinir
+// NO se podran redeclarar aunque podriamos hacer qu
+const booleano = false; // o true
+
+//booleano = numero  ** ERROR ILEGAL
+
+
+
+
+console.log("hello world");
+console.log(cadena);
+console.log(numero);
+console.log(booleano);
+
+console.log("/----------------------------------/")
